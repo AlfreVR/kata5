fb38ffaeb2d6c2fb55a84643c3709ae7a6b43a85
AlfredoVR <alfredo2.villarroya@gmail.com>
2024-01-15 13:44:43 +0000

    Added new Class FactorialCommand with new interface Command Added new dependency called spark-core in pom.xml

---
+++ src/main/java/software/ulpgc/kata5/FactorialCommand.java
@@ -0,0 +1,57 @@
+package software.ulpgc.kata5;
+
+import java.util.stream.IntStream;
+
+import static java.lang.Integer.parseInt;
+
+public class FactorialCommand implements Command{
+    private final static Output NullNumberOutput = nullNumberOutput();
+
+    private Output factorial(String number) {
+        Output result;
+        if(number != null) {
+            result = result(factorial(parseInt(number)));
+        } else{
+            result = NullNumberOutput;
+        }
+        return result;
+    }
+
+    private int factorial(int number) {
+        return  IntStream.range(2, number + 1).reduce(1, (a, b) -> a * b);
+    }
+
+    private Output result(int factorial) {
+        return new Output() {
+            @Override
+            public int response() {
+                return 200;
+            }
+
+            @Override
+            public String result() {
+                return String.valueOf(factorial);
+            }
+        };
+    }
+
+    @Override
+    public Output execute(Input input) {
+        return null;
+    }
+
+
+    private static Output nullNumberOutput() {
+        return new Output() {
+            @Override
+            public int response() {
+                return 0;
+            }
+
+            @Override
+            public String result() {
+                return null;
+            }
+        };
+    }
+}
